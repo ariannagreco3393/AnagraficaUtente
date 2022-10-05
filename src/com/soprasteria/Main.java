01ca18ddabee2de68d84511ddc0cf6b74a4429f2
GRECO Arianna <arianna.greco@soprasteria.com>
2022-10-05 15:40:30 +0200

    add main class

---
+++ src/com/soprasteria/Main.java
@@ -0,0 +1,15 @@
+package com.soprasteria;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String nome = "arianna";
+		String cognome = "greco";
+		String eta = "29";
+		String luogo = "cantu";
+		double altezza = 1.68;
+		boolean patente = true;
+		char genere = 'f';
+	}
+
+}
